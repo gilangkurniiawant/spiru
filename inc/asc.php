08418267c0628ddd0e7df79e93f84d43e1a02535
gilangkurniiawant <gkurniiawant@gmail.com>
2019-10-22 23:02:45 +0700

    https2

--- inc/asc.php
+++ inc/asc.php
@@ -1,5 +1,5 @@
 <?php
-if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+if (empty($_SERVER['HTTPS'])) {
     $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     header('HTTP/1.1 301 Moved Permanently');
     header('Location: ' . $location);
