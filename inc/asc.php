46825f467fa2693f526ce3ea462fff1f8af1620d
gilangkurniiawant <gkurniiawant@gmail.com>
2019-10-22 22:50:54 +0700

    https

--- inc/asc.php
+++ inc/asc.php
@@ -1,3 +1,11 @@
+<?php
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $location);
+    exit;
+}
+?>
 <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1.0">
 <link data-asynced='1' as='style' onload='this.rel="stylesheet"' type="text/css" media="all" href="css/1.css" rel="preload" />
 <title>Sirhuka - Temukan Cantik Alamimu Disini</title>
